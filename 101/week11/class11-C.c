16ef1421b54cf4a7a6db1077eee5e1e560a269a3
grassblock233 <w15067120025@outlook.com>
2024-11-25 19:00:07 +0800

    Update class11-C.c

--- 101/week11/class11-C.c
+++ 101/week11/class11-C.c
@@ -1,68 +1,84 @@
 #include <stdio.h>
-#include <stdlib.h>
-struct repeat
-{
-    int *ans;
-    int *count;
-    int size;
-};
-
-typedef struct repeat *Repeat;
-
-void init()
-{
-    Repeat p;
-    p->ans = malloc(sizeof(int) * 20);
-    p->count = malloc(sizeof(int) * 20);
-    if (p->ans == NULL)
-    {
-        return;
-    }
-    if (p->count == NULL)
-    {
-        return;
-    }
-    p->count = 0;
-    p->size = 0;
-}
-
-_Bool find(int index)
+_Bool IsRepeated(int a, int *repeat, int size)
 {
     int i;
-    Repeat p;
-    _Bool flag = 0;
-    for (i = 0; i < p->size; i++)
+    int flag = 0;
+    if (size == 0)
     {
-        if (index == p->ans[i])
+        return 0;
+    }
+    for (i = 0; i <= size; i++)
+    {
+        if (a == repeat[i])
         {
             flag = 1;
         }
     }
-    
+    return flag;
 }
 
-void Num(int *a)
+int locate(int a, int *repeat, int size)
 {
-    Repeat p;
     int i;
-    for (i = 0; i < 10; i++)
+    int loc;
+    for (i = 0; i <= size; i++)
     {
-        scanf("%d", (a + i));
+        if (a == repeat[i])
+        {
+            loc = i;
+        }
     }
-    for (i = 0; i < 10; i++)
-    {
+    return loc;
+}
 
+int max_count(int *count,int size)
+{
+    int max = count[0];
+    int i;
+    int loc = 0;
+    for (i = 1;i <= size;i++)
+    {
+        if (count[i] > max)
+        {
+            loc = i;
+            max = count[i];
+        }
     }
+    return loc;
+}
+
+int Num(int a[])
+{
+    int i ,k;
+    int repeat[20];
+    int count[20];
+    int size = 0;
+
+    for (i = 0; i < 10; i++)
+    {
+        if (IsRepeated(a[i], repeat, size) == 0)
+        {
+            count[size] = 1;
+            repeat[size] = a[i];
+            size++;
+        }
+        else
+        {
+            count[locate(a[i], repeat, size)]++;
+        }
+    }
+    printf("%d\n",repeat[max_count(count,size)]);
+    printf("%d\n",count[max_count(count,size)]);
 }
 
 int main()
 {
-    struct repeat RE;
-    int *a = malloc(sizeof(int) * 20);
-    if (a == NULL)
+    int a[20];
+    int i;
+    for (i = 0; i < 10; i++)
     {
-        return -1;
+        scanf("%d", &a[i]);
     }
-    init(&RE);
     Num(a);
+    return 0;
 }
